0544c36fc428c04735da02b8e30473f74a570f6c
my-U <myouth_u@naver.com>
2020-09-04 17:18:57 +0900

    js_practice_09_04

--- 09/09_04/checklist.js
+++ 09/09_04/checklist.js
@@ -10,4 +10,16 @@ function addList() {
         document.querySelector("#item").value = ""; // 준비물을 입력한 후 텍스트 필드의 내용을 지움
         document.querySelector("#item").focus(); // 텍스트 필드의 커서를 다시 활성화
     }
+    showList(); // 목록을 표시하는 showList() 함수 실행
 }
+
+function showList() {
+    var list = "<ul>"; // 목록을 시작하는 <ul> 태그 저장
+    for(var i = 0; i < itemList.length; i++){
+        list += "<li>" + itemList[i] + "</li>"; //각 요소를 <li> ~ </li>로 묶어 반복
+    }
+    list += "</ul>"; // 목록을 끝내는 </ul> 태그 저장
+    
+    document.querySelector("#itemList").innerHTML = list;
+}
+
