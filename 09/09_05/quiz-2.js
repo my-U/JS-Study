6c852039ce309b53b534213d011a96a59c06209b
my-U <myouth_u@naver.com>
2020-09-05 23:34:02 +0900

    js_practice_09_05

---
+++ 09/09_05/quiz-2.js
@@ -0,0 +1,14 @@
+var goodSayings = [];
+goodSayings[0] = "자존심은 어리석은 자의 소유물이다";
+goodSayings[1] = "후회를 최대한 이용하라. 깊이 후회한다는 것은 새로운 삶을 산다는 것이다.";
+goodSayings[2] = "후회를 최대한 이용하라. 깊이 후회한다는 것은 새로운 삶은 산다는 것이다.";
+goodSayings[3] = "가짜 친구는 소문을 믿고 진짜 친구는 나를 믿는다.";
+goodSayings[4] = "성공이라는 못을 박으려면 끈질김이라는 망치가 필요하다.";
+goodSayings[5] = "인생이란 결코 공평하지 않다. 이 사실에 익숙해져라.";
+goodSayings[6] = "'언젠가'라는 날은 영원히 오지 않는다.";
+goodSayings[7] = "문제점을 찾지 말고 해결책을 찾으라.";
+goodSayings[8] = "착한 일은 작다 해서 아니하지 말고, 악한 일은 작다 해도 하지 말라.";
+goodSayings[9] = "자존심은 어리석은 자의 소유물이다";
+
+var i = Math.floor(Math.random() * 10);
+document.write("&quot;" + goodSayings[i] + "&quot;");
