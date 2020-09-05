07c2d7cc655ad4b47f03e81ad4d6c576bdb7d9d4
my-U <myouth_u@naver.com>
2020-09-05 22:58:34 +0900

    js_practice_09_05

--- 09/09_05/quiz-1.js
+++ 09/09_05/quiz-1.js
@@ -2,10 +2,15 @@
 		showArray(numbers);
 
 		function showArray(arr) {
+			var sum = 0;
 			var str = "<table><tr>";
 			for (var i=0; i<arr.length; i++) {
 				str += "<td>" + arr[i] + "</td>";
 			}
+			for (var i = 0; i < arr.length; i++) {
+				sum += arr[i];
+			}
+			str += "<td>" + sum + "</td>";
 			str += "</tr></table>";
 			document.write(str);
 		}
