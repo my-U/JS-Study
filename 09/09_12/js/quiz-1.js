3aa97ed736d0b0efa6e7969159b3b9cccf0e4944
my-U <myouth_u@naver.com>
2020-09-12 17:43:49 +0900

    js_practice_09_12

---
+++ 09/09_12/js/quiz-1.js
@@ -0,0 +1,5 @@
+function openPop() {
+    var Win = window.open("current.html", "", "width=300, heigth=50");
+}
+
+window.onload = openPop;
---
+++ 09/09_12/js/quiz-2.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var backColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = backColor;
+}
+
+changeColor();
