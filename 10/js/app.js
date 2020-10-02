89ae238cfa7c5d0a339a4e4fe26efaa67fa8512d
my-U <myouth_u@naver.com>
2020-10-02 21:23:42 +0900

    js_practice_10_02

---
+++ 10/js/app.js
@@ -0,0 +1,41 @@
+var btn = document.querySelector("#generate-btn")
+var quote = document.querySelector("#quote")
+var author = document.querySelector(".author")
+var quoteList = [
+  {
+    quote:
+      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+    author: " Life"
+  },
+  {
+    quote:
+      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    author: "John Kenneth Galbraith"
+  },
+  {
+    quote:
+      "God save me from my friends. I can protect myself from my enemies.",
+    author: "Claude Louis Hector de Villars "
+  },
+  {
+    quote: "The price of anything is the amount of life you exchange for it.",
+    author: "David Thoreau"
+  },
+  {
+    quote:
+      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    author: "Charles Lindbergh"
+  },
+  {
+    quote:
+      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    author: " Tyne Daly"
+  }
+];
+
+btn.addEventListener("click", function() {
+  var index = Math.floor(Math.random() * quoteList.length);
+
+  quote.textContent = quoteList[index].quote;
+  author.textContent = quoteList[index].author;
+})
