4fbdd462ec908d32c1becc5212593bb8aa1ecdd5
my-U <myouth_u@naver.com>
2020-10-18 15:32:07 +0900

    js_practice_10_18_err_fix

--- 10/10_18/js/app.js
+++ 10/10_18/js/app.js
@@ -45,13 +45,13 @@
 })()*/
 
 (function() {
-    const storeItems = document.querySelectorAll('.store-item')
-    const storeImg = document.querySelectorAll('.store-img')
-    const lightBox = document.querySelector('.lightbox-container')
-    const lightBoxItem = document.querySelector('.lightbox-item')
+    let storeItems = document.querySelectorAll('.store-item')
+    let storeImg = document.querySelectorAll('.store-img')
+    let lightBox = document.querySelector('.lightbox-container')
+    let lightBoxItem = document.querySelector('.lightbox-item')
 
-    const imageList = [];
-    const imageCounter = 0;
+    let imageList = [];
+    let imageCounter = 0;
 
     storeImg.forEach(function(image) { // 담겨있는 모든 요소들이 순서대로 한번씩 함수를 호출함
         imageList.push(image.src) // 각 이미지를 리스트에 삽입
@@ -60,34 +60,34 @@
 
     storeItems.forEach(function(item) {
         item.addEventListener('click', (e) => {
-            const image = e.target.src // store-item 내에 있는 src 자동 타겟 지정(?)
-            lightBoxItem.style.backgourndImage = `url(${image})`
+            let image = e.target.src // store-item 내에 있는 src 자동 타겟 지정(?)
+            lightBoxItem.style.backgroundImage = `url(${image})`
             lightBox.classList.add('show') // lightBox의 클래스에 show 추가, 클래스가 show인 css가 작성되어 있음
             imageCounter = imageList.indexOf(image) // indexOf : 배열에서 지정된 항목을 검색하고 해당 위치를 반환
         }) // imageCounter에는 해당하는 이미지의 주소의 인덱스값이 들어있음
     })
 
 
-    const btnLeft = document.querySelector('.btnLeft')
+    let btnLeft = document.querySelector('.btnLeft')
     btnLeft.addEventListener('click', (e) => {
         imageCounter--
         if(imageCounter < 0) {
             imageCounter = imageList.length - 1
         }
-        imageBoxItem.style.backgourndImage  = `url(${imageList[imageCounter]})`
+        lightBoxItem.style.backgroundImage  = `url(${imageList[imageCounter]})`
     })
 
-    const btnRight = document.querySelector('.btnRight')
+    let btnRight = document.querySelector('.btnRight')
     btnRight.addEventListener('click', (e) => {
         imageCounter++
         if(imageCounter === imageList.length) {
             imageCounter = 0
         }
-        imageBoxItem.style.backgourndImage = `url(${imageList[imageCounter]})`
+        lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`
     })
 
-    const lightBoxClose = document.querySelector('.lightbox-close')
-    lightBoxItem.addEventListener('click', (e) => {
+    let lightBoxClose = document.querySelector('.lightbox-close')
+    lightBoxClose.addEventListener('click', (e) => {
         lightBox.classList.remove('show')
     })
 })()
