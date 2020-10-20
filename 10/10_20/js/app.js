8eeef31e60882007ba7ebcf7d9e9238346e47746
my-U <myouth_u@naver.com>
2020-10-20 23:56:15 +0900

    js_practice_10_20

--- 10/10_20/js/app.js
+++ 10/10_20/js/app.js
@@ -1,4 +1,4 @@
-(function(){
+/*(function(){
   let bill = document.querySelector('#input-bill')
   let users = document.querySelector('#input-users')
   let service = document.querySelector('#input-service')
@@ -28,4 +28,40 @@
     }
   }) // 모르겠다
 
+})();*/
+
+(function() {
+  const services = [ // 드롭다운 리스트
+    {
+      value : 1,
+      title : 'great - 20%'
+    },
+    {
+      value : 2,
+      title : 'good - 10%'
+    },
+    {
+      value : 3,
+      title : 'bad - 2%'
+    }
+  ]
+  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+
+    const inputBill = document.querySelector('#input-bill');
+    const inputUsers = document.querySelector('#input-users');
+    const serviceValue = document.querySelector('#input-service');
+
+    let billAmount = inputBill.value
+    let usersAmount = inputUsers.value
+    let selectedService = serviceValue.value
+
+    
+  })
+
+  
 })();
