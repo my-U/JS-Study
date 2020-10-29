6a4493e97d09b4f8bd0e3f8519e6d922823ad4a7
my-U <myouth_u@naver.com>
2020-10-29 20:45:30 +0900

    js_practice_10_29

--- 10/10_20/js/app.js
+++ 10/10_20/js/app.js
@@ -44,11 +44,49 @@
       value : 3,
       title : 'bad - 2%'
     }
-  ]
+  ]  
+  
+  services.forEach(function(service) { // 리스트에 저장되어 있는 데이터가 드롭다운의 값으로 만들어짐
+    const option = document.createElement('option')
+    const select = document.querySelector('#input-service')
+
+    option.value = service.value
+    option.textContent = service.title
+
+    select.appendChild(option)
+  })
+
+  function validateInput(billAmount, usersAmount, selectedService) { // 값이 유효한지 확인하는 함수
+    let isFeedback = false
+    const feedback = document.querySelector('.feedback') // 빈값이 넘어갔을 경우 경고 메세지가 뜨는 공간
+    feedback.innerHTML = ''
+
+    if(billAmount === '' || billAmount <= '0') {
+      feedback.classList.add('showItem', 'alert-danger')
+      feedback.innerHTML += '<p>음식값을 입력해주세요.</p>'
+      isFeedback = true
+    }
+
+    if(usersAmount === '' || usersAmount <= '0') {
+      feedback.classList.add('showItem', 'alert-danger')
+      feedback.innerHTML += '<p>인원수를 입력해주세요.</p>'
+      isFeedback = true
+    }
+
+    if (selectedService === "0"){
+      feedback.classList.add('showItem', 'alert-danger');
+      feedback.innerHTML += `<p>서비스를 선택해주세요.</p>`
+       isFeedback = true;
+    }
+
+    setTimeout(function() {
+      feedback.classList.remove('showItem', 'alert-danger')
+    }, 10000) // 10초 후 경고메세지 삭제
+  }
   
   const inputForm = document.querySelector('form');
 
-  inputForm.addEventListener('submit', function(e) {
+  inputForm.addEventListener('submit', function(e) { // 폼에서 값이 넘어가면
 
     e.preventDefault();
 
@@ -59,6 +97,8 @@
     let billAmount = inputBill.value
     let usersAmount = inputUsers.value
     let selectedService = serviceValue.value
+
+    const isFeedback = validateInput(billAmount, usersAmount, selectedService)
 
     
   })
