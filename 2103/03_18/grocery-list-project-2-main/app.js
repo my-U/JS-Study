80925dce1bad9cba7ad65333a452bcc7b0cce4b1
my-U <myouth_u@naver.com>
2021-03-20 23:40:29 +0900

    Add Delete Alert

--- 2103/03_18/grocery-list-project-2-main/app.js
+++ 2103/03_18/grocery-list-project-2-main/app.js
@@ -3,6 +3,8 @@ const $itemInput = document.querySelector(".addItems-input");
 const $groceryItemList = document.querySelector(".grocery-list");
 const $deleteBtns = document.querySelectorAll(".grocery-item__link");
 const $allClearItem = document.querySelector(".displayItems-clear");
+const $feedback = document.querySelector(".addItems-action");
+const $dltFeedback = document.querySelector(".displayItems-action");
 
 let itemValueList = [];
 
@@ -19,12 +21,13 @@ const addListItem = (e) => {
     const inputValue = $itemInput.value;
 
     if(inputValue === "") {
-        return 0;
+        showFeedback("alert", "Please Add Grocery Item!");
     }
     else {
         itemValueList.push(inputValue);
         showListItem(inputValue);
         $itemInput.value = "";
+        showFeedback("success", `${inputValue} Added To The List`);
     }
 }
 
@@ -43,10 +46,16 @@ const deleteItem = ({target}) => {
     if(target.parentElement.classList.contains("grocery-item__link")) {
         const deleteBtn = target.closest("div");
         $groceryItemList.removeChild(deleteBtn);
-
+        
         itemValueList.filter((item) => {
-            return item !== target.parentElement.previousSibling.textContent;
+            return item !== target.parentElement.previousSibling.previousSibling.textContent;
         });
+        // console.log(itemValueList);
+        $dltFeedback.classList.add("success");
+        $dltFeedback.innerHTML = `<p>${target.parentElement.previousSibling.previousSibling.textContent} Removed From List</p>`;
+        setTimeout(() => {
+            $dltFeedback.classList.remove("success");
+        }, 3000);
     }
 }
 
@@ -54,6 +63,15 @@ const allClear = ({target}) => {
     if(target.classList.contains("displayItems-clear")){
         $groceryItemList.innerHTML = `<button type="submit" class="displayItems-clear">clear items</button>`;
     }
+}
+
+const showFeedback = (state, text) => {
+    $feedback.classList.add(state);
+    $feedback.innerHTML = `<p>${text}</p>`;
+
+    setTimeout(() => {
+        $feedback.classList.remove("success", "alert");
+    }, 3000);
 }
 
 init();
